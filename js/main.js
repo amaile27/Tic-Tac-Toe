2734c8706ad72999d10b55d7b5034118d1b53909
amaile <ajamaile@gmail.com>
2022-07-31 11:08:48 -0700

    sunday work

--- js/main.js
+++ js/main.js
@@ -1,29 +1,13 @@
-// 1) Define required constants:
-// 	1.1) Define a colors object with keys of 'null' (when the square is empty), and players 1 & -1. The value assigned to each key represents the color to display for an empty square (null), player 1 and player -1.
-// 	1.2) Define the 8 possible winning combinations, each containing three indexes of the board that make a winner if they hold the same player value.
 
-
-//1.
-const colors = {
-    null: 'red',
-    playerOne: 'black',
-    playerTwo: 'white' 
-};
-
-//1.2 Thinking I'll make an object of win option objects that contain strings of all 8 winning possibilities.
-// const winningCombos = {
-//    winOne: ('one', 'two', 'three'),
-//    winTwo: ('four', 'five', 'six'),
-//    winThree: ('seven', 'eight', 'nine'),
-//    winFour: ('one', 'four', 'seven'),
-//    winFive: ('two', 'five', 'eight'),
-//    winSix: ('three', 'six', 'nine'),
-//    winSeven: ('one', 'five', 'nine'),
-//    winEight: ('three', 'five', 'seven'),
-// }
-// probably better done with arrays?
-
-const winningCombos = [
+//1.1 define colors object
+const colors {
+    'null': 'black',
+    'player1': 'pink',
+    'player2': 'dodgerblue',
+  }
+  
+  //1.2 define winning combinations
+  const winningCombos = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
@@ -31,17 +15,34 @@ const winningCombos = [
     [2, 5, 8],
     [3, 6, 9],
     [1, 5, 9],
-    [3, 5, 7],
+    [3, 5, 7]
   ];
+  
+//2.1 create board array
+  let board = [];
+//2.2 create turn variable
+  let playerTurn;
+//2.3 create winner variable
+  let winner;
+  
+  //4.1 initialize board array, set winner to null, set to start with player1 (x)
+  initGame();
+
+  function initGame() {
+    board = [
+      "", "", "",
+      "", "", "",
+      "", "", ""
+    ];
+    winner = null;
+    playerTurn = 'player1';
+    render();
+  }
+
+//4.2
 
 
-// 2) Define required variables used to track the state of the game:
-// 	2.1) Use a board array to represent the squares.	2.2) Use a turn variable to remember whose turn it is.
-// 	2.3) Use a winner variable to represent three different possibilities - player that won, a tie, or game in play.
 
-// 2.1
-const boardArr = [
-
-]
-
-let turn = 
+  
+  
+  
